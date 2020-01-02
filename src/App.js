d11659c14ee2f567e3631ac1c651cf1c17ac4854
felixongati <fegati02@gmail.com>
2020-01-02 13:50:09 +0300

    conditional inline styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TodoItem from './TodoItem'
 import todosData from './todosData'
 import './styles.css';
 
-function App() {
-  const todoComponents = todosData.map(item => {
-    return <TodoItem key={item.id} list={item}/>
+class App extends Component {
+
+  constructor(){
+    super()
+
+    this.state={
+      todos:todosData
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+handleChange(id){
+  this.setState((prevState)=>{
+    const updatedTodos = prevState.todos.map(item => {
+      if (item.id === id){
+        return{
+          ...item,
+          completed:!item.completed
+        }
+      }
+      return item
+    })
+    return {
+      todos:updatedTodos
+    }
   })
-  return (
-    <div className="todo-list">
-     {todoComponents}
-    </div>
-  );
+}
+
+
+  render(){
+    const todoComponents = this.state.todos.map(item => { return <TodoItem key={item.id} list={item} handleChange={this.handleChange}/>})
+    return (
+      <div className="todo-list">
+      {todoComponents}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './styles.css';
 
 function TodoItem(props){
+    const markCompleted = {
+        fontStyle: "italic",
+        color: "red",
+        textDecoration: "line-through"
+    }
     return(
         <div className="todo-item">
-            <input type="checkbox" checked={props.list.completed}/>
-            <p>{props.list.text}</p>
+            <input type="checkbox" checked={props.list.completed} onChange={()=>props.handleChange(props.list.id)}/>
+            <p style = {props.list.completed ? markCompleted:null}>{props.list.text}</p>
         </div>
     )
 }
